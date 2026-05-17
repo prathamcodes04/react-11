31d1565a93f165433b9a0e7cbf44f1afce6c1789
Pratham Gupta <prathamgupta0405@gmail.com>
2026-05-17 17:35:14 +0530

    move order and tracking page to react

--- ecom-project/src/App.jsx
+++ ecom-project/src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} /> 
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} /> 
+      <Route path="tracking" element={<TrackingPage />} /> 
     </Routes>
   )
 }                                          
