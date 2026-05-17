d9624785b60e5dfabcbcc4cb982a415fc23f69d7
Pratham Gupta <prathamgupta0405@gmail.com>
2026-05-17 22:37:13 +0530

    Refactor code structure for improved readability and maintainability

--- ecom-project/src/pages/CheckoutPage.jsx
+++ ecom-project/src/pages/CheckoutPage.jsx
@@ -5,6 +5,7 @@ export function CheckoutPage(){
     return (
         <>
             <title>Checkout</title>
+            <link rel="icon" type="image/svg+xml" href="cart-favicon.png" />
 
             <div className="checkout-header">
                 <div className="header-content">
