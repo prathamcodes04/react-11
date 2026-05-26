d43c8f28c031c755a26529a9d180b7f0668e7e31
Pratham Gupta <prathamgupta0405@gmail.com>
2026-05-26 13:47:28 +0530

    convert hom, order and checkout to async/await

--- ecom-project/src/App.jsx
+++ ecom-project/src/App.jsx
@@ -1,32 +1,34 @@
-import { Routes, Route } from 'react-router'
-import { useEffect, useState } from 'react'
-import axios from 'axios';
-import { HomePage } from './pages/home/HomePage'
-import { CheckoutPage } from './pages/checkout/CheckoutPage'
-import { OrdersPage } from './pages/orders/OrdersPage';
-import { TrackingPage } from './pages/TrackingPage'
-import { NotFoundPage } from './pages/pagenotfound/NotFoundPage'
-import './App.css'
+import { Routes, Route } from "react-router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { HomePage } from "./pages/home/HomePage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { OrdersPage } from "./pages/orders/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
+import { NotFoundPage } from "./pages/pagenotfound/NotFoundPage";
+import "./App.css";
 
 function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/cart-items?expand=product')
-      .then((response) => {
-        setCart(response.data);
-      })
-   }, [])
+   const fetchAppData = async () => {
+      const response = await axios.get("/api/cart-items?expand=product");
+      setCart(response.data);
+    };
+
+    fetchAppData();
+  }, []);
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage cart={cart}/>} />
-      <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
-      <Route path="orders" element={<OrdersPage cart={cart}/>} /> 
-      <Route path="tracking" element={<TrackingPage />} /> 
-      <Route path="*" element={<NotFoundPage />} />  
+      <Route path="/" element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
+      <Route path="tracking" element={<TrackingPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
-  )
-}                                          
+  );
+}
 
-export default App
+export default App;
--- ecom-project/src/pages/home/HomePage.jsx
+++ ecom-project/src/pages/home/HomePage.jsx
@@ -1,29 +1,31 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import "./HomePage.css";
-import { ProductsGrid } from './ProductsGrid';
+import { ProductsGrid } from "./ProductsGrid";
 
-export function HomePage({cart}) {
-   const [products, setProducts] = useState([]);
+export function HomePage({ cart }) {
+  const [products, setProducts] = useState([]);
 
-   useEffect(() => {
-      axios.get('/api/products')
-         .then((response) => {
-            setProducts(response.data);
-         });
-   }, [])
+  useEffect(() => {
+    const getHomeData = async () => {
+      const response = await axios.get("/api/products");
+      setProducts(response.data);
+    };
 
-   return (
-      <>
-         <title>Ecommerce Project</title>
-         <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+    getHomeData();
+  }, []);
 
-         <Header cart={cart}/>
+  return (
+    <>
+      <title>Ecommerce Project</title>
+      <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
 
-         <div className="home-page">
-            <ProductsGrid products={products}/>
-         </div>
-      </>
-   );
+      <Header cart={cart} />
+
+      <div className="home-page">
+        <ProductsGrid products={products} />
+      </div>
+    </>
+  );
 }
