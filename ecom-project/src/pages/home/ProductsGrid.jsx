5ad5ba62a52bcec55c4ca71abee03a5a4e869bd5
Pratham Gupta <prathamgupta0405@gmail.com>
2026-05-30 21:15:04 +0530

    feat: add quantity selection functionality in ProductsGrid component

--- ecom-project/src/pages/home/ProductsGrid.jsx
+++ ecom-project/src/pages/home/ProductsGrid.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import { useState } from "react";
 
 export function ProductsGrid({ products, loadCart }) {
+  const [quantity, setQuantity] = useState(1);
+
   return (
     <div className="products-grid">
       {products.map((product) => {
@@ -30,7 +33,11 @@ export function ProductsGrid({ products, loadCart }) {
             </div>
 
             <div className="product-quantity-container">
-              <select>
+              <select value={quantity} onChange={(event) => {
+                const selectedQuantity = Number(event.target.value);
+                // console.log(selectedQuantity);
+                setQuantity(selectedQuantity);
+              }}>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
