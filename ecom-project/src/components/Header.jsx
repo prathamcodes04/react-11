0d419a565c22ee8112c424ef8dceaca40330a81a
Pratham Gupta <prathamgupta0405@gmail.com>
2026-06-06 23:46:25 +0530

    implement search functionality in Header, add quantity update on Enter key in CartItemDetails, and enhance product fetching in HomePage

--- ecom-project/src/components/Header.jsx
+++ ecom-project/src/components/Header.jsx
@@ -1,13 +1,28 @@
-import { Link } from 'react-router';
-import './header.css';
-import logoWhite from '../assets/images/logo-white.png';
+import { Link, useNavigate, useSearchParams } from "react-router";
+import "./header.css";
+import logoWhite from "../assets/images/logo-white.png";
+import { useState } from "react";
 
-export function Header({cart = []}) {
+export function Header({ cart = [] }) {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const searchText = searchParams.get("search");
+
+  const [search, setSearch] = useState(searchText || '');
 
   let totalQuantity = 0;
   cart.forEach((cartItem) => {
     totalQuantity += cartItem.quantity;
   });
+
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchProducts = () => {
+    navigate(`/?search=${search}`);
+  };
 
   return (
     <div className="header">
@@ -19,10 +34,19 @@ export function Header({cart = []}) {
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search"
+          value={search}
+          onChange={updateSearchInput}
+        />
 
-        <button className="search-button">
-          <img className="search-icon" src="src/assets/images/icons/search-icon.png" />
+        <button className="search-button" onClick={searchProducts}>
+          <img
+            className="search-icon"
+            src="src/assets/images/icons/search-icon.png"
+          />
         </button>
       </div>
 
@@ -32,7 +56,10 @@ export function Header({cart = []}) {
         </Link>
 
         <Link className="cart-link header-link" to="/checkout">
-          <img className="cart-icon" src="src/assets/images/icons/cart-icon.png" />
+          <img
+            className="cart-icon"
+            src="src/assets/images/icons/cart-icon.png"
+          />
           <div className="cart-quantity">{totalQuantity}</div>
           <div className="cart-text">Cart</div>
         </Link>
--- ecom-project/src/pages/checkout/CartItemDetails.jsx
+++ ecom-project/src/pages/checkout/CartItemDetails.jsx
@@ -27,6 +27,16 @@ export function CartItemDetails({ cartItem, loadCart }) {
     setQuantity(event.target.value);
   }
 
+  const handleQuantityKeyDown = (event) => {
+    const keyPressed = event.key;
+    if(keyPressed === 'Enter'){
+      updateQuantity();
+    }else if(keyPressed === 'Escape'){
+      setQuantity(cartItem.quantity);
+      isUpdatingQuantity(false);
+    }
+  }
+
   return (
     <>
       <img className="product-image" src={cartItem.product.image} />
@@ -42,6 +52,7 @@ export function CartItemDetails({ cartItem, loadCart }) {
               ? <input type="text" className="quantity-textbox"
                   value={quantity}
                   onChange={updateQuantityInput}
+                  onKeyDown={handleQuantityKeyDown}
                 />
               : <span className="quantity-label">{cartItem.quantity}</span>
             }
