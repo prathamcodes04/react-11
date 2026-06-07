5c712c0cb0e2b7e71ce0fe4b893054d40cc58118
Pratham Gupta <prathamgupta0405@gmail.com>
2026-06-08 02:38:59 +0530

    test clicking add to cart button

--- ecom-project/src/pages/home/Products.jsx
+++ ecom-project/src/pages/home/Products.jsx
@@ -75,6 +75,7 @@ export function Products({ product, loadCart }) {
       </div>
 
       <button className="add-to-cart-button button-primary" 
+        data-testid = "add-to-cart-button"
         onClick={addToCart}>
         Add to Cart
       </button>
