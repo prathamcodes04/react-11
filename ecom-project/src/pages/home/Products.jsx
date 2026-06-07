4ea49f0a6b056f1ccbcb68faa6414ecf2a804e63
Pratham Gupta <prathamgupta0405@gmail.com>
2026-06-08 01:58:07 +0530

    create integration test for product component

--- ecom-project/src/pages/home/Products.jsx
+++ ecom-project/src/pages/home/Products.jsx
@@ -29,7 +29,9 @@ export function Products({ product, loadCart }) {
   return (
     <div className="product-container">
       <div className="product-image-container">
-        <img className="product-image" src={product.image} />
+        <img className="product-image" 
+        data-testid = "product-image"
+        src={product.image} />
       </div>
 
       <div className="product-name limit-text-to-2-lines">{product.name}</div>
@@ -37,9 +39,11 @@ export function Products({ product, loadCart }) {
       <div className="product-rating-container">
         <img
           className="product-rating-stars"
+          data-testid = "product-rating-stars-image"
           src={`images/ratings/rating-${product.rating.stars * 10}.png`}
         />
-        <div className="product-rating-count link-primary">
+        <div className="product-rating-count link-primary"
+        >
           {product.rating.count}
         </div>
       </div>
